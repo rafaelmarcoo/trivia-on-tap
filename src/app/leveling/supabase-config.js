7092de143e06961ee3a0ff53dcfcd5f35dde30bc
Kaiden Ziegler <dxv5648@autuni.ac.nz>
2025-04-30 11:11:47 +1200

    finish copy

--- src/app/leveling/supabase-config.js
+++ src/app/leveling/supabase-config.js
@@ -1,7 +1,9 @@
+"use client";
+
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseKey = import.meta.env.SUPABASE_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
